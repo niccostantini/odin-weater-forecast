ae9b47cbb493197906e1ce0a5dac7178c50e6c5d
niccostantini <65295160+niccostantini@users.noreply.github.com>
2024-10-17 15:56:47 +0200

    Create logic for showing images based on weather

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import "./styles.css";
 import "./normalize.css";
 
+const weatherImages = {
+  "clear": "https://raw.githubusercontent.com/niccostantini/odin-weater-forecast/refs/heads/main/src/assets/images/clear.webp",
+  "cloudy": "https://raw.githubusercontent.com/niccostantini/odin-weater-forecast/refs/heads/main/src/assets/images/clody.webp",
+  "cloudRainy": "https://raw.githubusercontent.com/niccostantini/odin-weater-forecast/refs/heads/main/src/assets/images/cloudy-rainy.webp",
+  "overcast": "https://raw.githubusercontent.com/niccostantini/odin-weater-forecast/refs/heads/main/src/assets/images/overcast.webp",
+  "partiallyCloudyRainy": "https://raw.githubusercontent.com/niccostantini/odin-weater-forecast/refs/heads/main/src/assets/images/partially-cloudy-rainy.webp",
+  "partiallyCloudy": "https://raw.githubusercontent.com/niccostantini/odin-weater-forecast/refs/heads/main/src/assets/images/partially-cloudy.webp",
+  "snowy": "https://raw.githubusercontent.com/niccostantini/odin-weater-forecast/refs/heads/main/src/assets/images/snowy.webp",
+  "stormy": "https://raw.githubusercontent.com/niccostantini/odin-weater-forecast/refs/heads/main/src/assets/images/stormy.webp"
+}
+
 const resettableFields = [
   document.querySelector(".location"),
   document.querySelector(".date"),
@@ -49,23 +60,62 @@ function displayData(data) {
   const temperatureElement = document.querySelector(".temperature .text");
   const descriptionElement = document.querySelector(".description");
 
+  let icon = "";
+  let imgUrl = "";
+
+  //change bgimage based on weather conditions
+  let dataContitionsLC = data.conditions.toLowerCase();
+  if (dataContitionsLC == "clear") {
+    imgUrl = weatherImages.clear;
+    icon = "☀️";
+  } else if (dataContitionsLC == "cloudy") {
+    imgUrl = weatherImages.cloudy;
+    icon = "☁️";
+  } else if (dataContitionsLC == "rain, partially cloudy") {
+    imgUrl = weatherImages.partiallyCloudyRainy;
+    icon = "🌦️";
+  } else if (dataContitionsLC == "overcast") {
+    imgUrl = weatherImages.overcast;
+    icon = "😶‍🌫️";
+  }   else if (dataContitionsLC == "partially cloudy") {
+    imgUrl = weatherImages.partiallyCloudy;
+    icon = "⛅";
+  } else if (dataContitionsLC.includes("snow")) {
+    imgUrl = weatherImages.snowy;
+    icon = "❄️";
+  } else if (dataContitionsLC == "rain") {
+    imgUrl = weatherImages.cloudRainy;
+    icon = "🌧️";
+  } else if (dataContitionsLC == "thunderstorm") {
+    imgUrl = weatherImages.stormy;
+    icon = "⛈️";
+  } else if (dataContitionsLC == "rain, overcast") {
+    imgUrl = weatherImages.cloudRainy;
+    icon = "🌧️";
+  } else {
+    imgUrl = weatherImages.clear;
+    icon = "🌤️";
+  }
+
+  info.style.backgroundImage = `linear-gradient(to bottom, rgba(224, 224, 224, 0.3), rgba(234, 234, 234, 0.9)), url('${imgUrl}')`;
+
   const toCelsius = ((data.temp - 32) * (5 / 9)).toFixed(2);
 
   resetDisplayedData(resettableFields);
 
   locationElement.textContent = data.address;
   dateElement.textContent = data.datetime;
-  weatherElementIcon.textContent = "🌤️";
+  weatherElementIcon.textContent = icon;
   weatherElementText.textContent = `${data.conditions}`;
   temperatureElement.innerHTML = `${toCelsius}°C`;
   descriptionElement.textContent = data.description;
 }
 
-async function getWeatherData() {
+async function getWeatherData(index = 0) {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    let n = 0;
+    let n = index;
     let newData = {
       address: data.address,
       datetime: data.days[n].datetime,
@@ -73,7 +123,7 @@ async function getWeatherData() {
       temp: data.days[n].temp,
       description: data.days[n].description
     };
-    console.log(data);
+    console.log(newData);
     displayData(newData);
     if (info.classList.contains("hide")) {
       toggleVisibility(info);
@@ -92,9 +142,14 @@ toggleVisibility(info) //hide #info on page load
 //query the API through the form
 const form = document.querySelector("form");
 form.addEventListener("submit", function (event) {
+  //retrieve index corresponding to selected day
+  const dayElement = document.querySelector("#day");
+  let dayIndex = dayElement.options[dayElement.selectedIndex].id;
+  console.log(dayIndex);
+
   event.preventDefault();
   city = form.elements.city.value;
   country = form.elements.country.value;
   url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city},${country}?key=${key}`;
-  getWeatherData();
+  getWeatherData(dayIndex);
 });
